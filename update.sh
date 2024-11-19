774652839aca29a0fad4f74e3876710ff7098b91
fratardi <fratardi@students.42.fr>
2024-11-19 19:10:28 +0100

    not ok

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/dzserver/servers/steamcmd/steamcmd.sh +force_install_dir /home/dzserver/servers/dayz-server/ +login greed0w +app_update 223350 +quit
+/home/dzserver/servers/steamcmd/steamcmd.sh +force_install_dir /home/dzserver/servers/dayz-server/ +login your_login +app_update 223350 +quit
